1419867b0de4f384e7c3c4fe077e0ce3fda58750
abdulhannanm <abdulhannanm85@gmail.com>
2023-08-17 02:28:38 -0700

    added room id db and reg db

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.datastructures import URL
 from pydantic import BaseModel
+import mysql.connector
+import random
+import string
 
 
 app = FastAPI()
@@ -12,12 +15,41 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
 
-#from fastapi import FastAPI
+db_config = {
+    "host" : "localhost",
+    "user" : "root",
+    "password": "ffrn1234",
+}
 
-#app = FastAPI()
+
+
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
+
+
+def create_db():
+    cursor.execute("CREATE DATABASE IF NOT EXISTS chat_db")
+    cursor.execute("USE chat_db")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vid_ids (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        room_id TEXT
+    )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            message TEXT,
+            video_timestamp INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+create_db()
+
+
 
 class ConnectionManager:
     def __init__(self):
@@ -33,6 +65,22 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
+
+def add_room_id():
+    nor_res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
+    res = str(nor_res)
+    query1 = "select count(*) from vid_ids where room_id = %s"
+    values = (res, )
+    cursor.execute(query1, values)
+    result = cursor.fetchone()
+    if result[0] > 0:
+        add_room_id()
+    else:
+        query2 = "INSERT INTO vid_ids (room_id) VALUES(%s)"
+        values = (res, )
+        cursor.execute(query2, values)
+        connection.commit()
+    return res
 
 
 @app.websocket("/ws/{client_id}")
@@ -70,10 +118,11 @@ async def handle_form(video_title: str = Form(...), url: str = Form(...), profna
     new_url = ''.join(list_comp[32:43])
     print(new_url)
     print(profname)
-    return '/video/'+profname+"/"+video_title+"/"+new_url
+    room_id = add_room_id()
+    return '/video/'+profname+"/"+video_title+"/"+new_url+'/'+room_id
 
-@app.get("/video/{profname}/{video_title}/{new_url}", response_class=HTMLResponse)
-async def read_item(request: Request, profname: str, video_title: str, new_url: str):
+@app.get("/video/{profname}/{video_title}/{new_url}/{room_id}", response_class=HTMLResponse)
+async def read_item(request: Request, profname: str, video_title: str, new_url: str, room_id: str):
     video_url = "https://www.youtube.com/embed/" + new_url + "?enablejsapi=1"
     print(video_url)
     return templates.TemplateResponse("video.html", {"request": request, "video_title": video_title, "profname": profname, "url": video_url})
