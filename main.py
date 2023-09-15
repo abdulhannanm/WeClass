636ec232d784926848a65ec5486d90eeb1e87fc1
abdulhannanm <abdulhannanm85@gmail.com>
2023-09-15 08:57:45 -0700

    sve

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-db_config = {
-    "host" : "localhost",
-    "user" : "root",
-    "password": "ffrn1234",
-}
+# db_config = {
+#     "host" : "localhost",
+#     "user" : "root",
+#     "password": "ffrn1234",
+# }
 
 
 
-connection = mysql.connector.connect(**db_config)
-cursor = connection.cursor()
+# connection = mysql.connector.connect(**db_config)
+# cursor = connection.cursor()
 
 
 def create_db():
@@ -40,7 +40,7 @@ def create_db():
     )
     """)
 
-create_db()
+# create_db()
 
 
 
@@ -66,36 +66,36 @@ room_dict = {}
 def add_room_id():
     nor_res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
     res = str(nor_res)
-    query1 = "select count(*) from vid_ids where room_id = %s"
-    values = (res, )
-    cursor.execute(query1, values)
-    result = cursor.fetchone()
-    if result[0] > 0:
-        add_room_id()
-    else:
-        query2 = "INSERT INTO vid_ids (room_id) VALUES(%s)"
-        values = (res, )
-        cursor.execute(query2, values)
-        create_table_query = f"""
-        CREATE TABLE IF NOT EXISTS {res} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            message TEXT,
-            video_timestamp INT,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        """
-        cursor.execute(create_table_query)
-        connection.commit()
+    # query1 = "select count(*) from vid_ids where room_id = %s"
+    # values = (res, )
+    # cursor.execute(query1, values)
+    # result = cursor.fetchone()
+    # if result[0] > 0:
+    #     add_room_id()
+    # else:
+    #     query2 = "INSERT INTO vid_ids (room_id) VALUES(%s)"
+    #     values = (res, )
+    #     cursor.execute(query2, values)
+    #     create_table_query = f"""
+    #     CREATE TABLE IF NOT EXISTS {res} (
+    #         id INT AUTO_INCREMENT PRIMARY KEY,
+    #         message TEXT,
+    #         video_timestamp INT,
+    #         timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    #     )
+    #     """
+    #     cursor.execute(create_table_query)
+    #     connection.commit()
     return res
 
             
 
 
-def get_messages_for_room(room_id):
-    query = f'SELECT * FROM {room_id}'
-    cursor.execute(query)
-    results = cursor.fetchall()
-    return results
+# def get_messages_for_room(room_id):
+#     query = f'SELECT * FROM {room_id}'
+#     cursor.execute(query)
+#     results = cursor.fetchall()
+#     return results
 
 @app.websocket("/ws/{room_id}/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str, room_id: str):
@@ -158,14 +158,18 @@ async def handle_form(video_title: str = Form(...), url: str = Form(...), profna
     print(new_url)
     print(profname)
     room_id = add_room_id()
-    return '/video/'+profname+"/"+video_title+"/"+new_url+'/'+room_id
+    print(room_id)
+    redirectUrl = '/video/'+profname+"/"+video_title+"/"+new_url+'/'+room_id
+    print(redirectUrl)
+    return redirectUrl
 
 
 @app.get("/video/{profname}/{video_title}/{new_url}/{room_id}", response_class=HTMLResponse)
 async def read_item(request: Request, profname: str, video_title: str, new_url: str, room_id: str):
     video_url = "https://www.youtube.com/embed/" + new_url + "?enablejsapi=1"
-    messages = get_messages_for_room(room_id)
-    print(messages)
+    # messages = get_messages_for_room(room_id)
+    # print(messages)
+    messages = '';
     return templates.TemplateResponse("video.html", 
     {"request": request, 
     "video_title": video_title, 
