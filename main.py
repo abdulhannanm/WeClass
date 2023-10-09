a87e57bb2164192f23523c3feb68103a7eddd364
abdulhannanm <abdulhannanm85@gmail.com>
2023-10-09 13:32:42 -0700

    finished db work

--- main.py
+++ main.py
@@ -7,54 +7,57 @@ import random
 import string
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import mysql
+import mysql.connector
+import json
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.middleware("http")
-async def add_permissions_policy_header(request: Request, call_next):
-    response: Response = await call_next(request)
-    response.headers["Permissions-Policy"] = "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
+# @app.middleware("http")
+# async def add_permissions_policy_header(request: Request, call_next):
+#     response: Response = await call_next(request)
+#     response.headers["Permissions-Policy"] = "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
 
-    return response
+#     return response
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-# db_config = {
-#     "host" : "localhost",
-#     "user" : "root",
-#     "password": "ffrn1234",
-# }
+db_config = {
+    "host" : "localhost",
+    "user" : "root",
+    "password": "ffrn1234",
+}
 
 
 
-# connection = mysql.connector.connect(**db_config)
-# cursor = connection.cursor()
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
 
 
 
 
 
-# def create_db():
-#     cursor.execute("CREATE DATABASE IF NOT EXISTS chat_db")
-#     cursor.execute("USE chat_db")
-#     cursor.execute("""
-#     CREATE TABLE IF NOT EXISTS vid_ids (
-#         id INT AUTO_INCREMENT PRIMARY KEY,
-#         room_id TEXT
-#     )
-#     """)
+def create_db():
+    cursor.execute("CREATE DATABASE IF NOT EXISTS chat_db")
+    cursor.execute("USE chat_db")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vid_ids (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        room_id TEXT
+    )
+    """)
 
-# create_db()
+create_db()
 
 
 
@@ -72,35 +75,55 @@ class Room:
     async def broadcast(self, message, sender):
         for client_id, connection in self.connections.items():
             print(message)
+            parsed_data = json.loads(message)
+            roomID = parsed_data.get("roomID")
+            timestamp = parsed_data.get('timestamp')
+            msg = parsed_data.get('message')
+            clientID = parsed_data.get('clientID')
+            add_messages_to_room(roomID, timestamp, msg, clientID)
             await connection.send_text(message)
 
 
 room_dict = {}
 
+
+
+
 def add_room_id():
     nor_res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
     res = str(nor_res)
-    # query1 = "select count(*) from vid_ids where room_id = %s"
-    # values = (res, )
-    # cursor.execute(query1, values)
-    # result = cursor.fetchone()
-    # if result[0] > 0:
-    #     add_room_id()
-    # else:
-    #     query2 = "INSERT INTO vid_ids (room_id) VALUES(%s)"
-    #     values = (res, )
-    #     cursor.execute(query2, values)
-    #     create_table_query = f"""
-    #     CREATE TABLE IF NOT EXISTS {res} (
-    #         id INT AUTO_INCREMENT PRIMARY KEY,
-    #         message TEXT,
-    #         video_timestamp INT,
-    #         timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    #     )
-    #     """
-    #     cursor.execute(create_table_query)
-    #     connection.commit()
+    query1 = "select count(*) from vid_ids where room_id = %s"
+    values = (res, )
+    cursor.execute(query1, values)
+    result = cursor.fetchone()
+    if result[0] > 0:
+        add_room_id()
+    else:
+        query2 = "INSERT INTO vid_ids (room_id) VALUES(%s)"
+        values = (res, )
+        cursor.execute(query2, values)
+        create_table_query = f"""
+        CREATE TABLE IF NOT EXISTS {res} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            clientid TEXT,
+            message TEXT,
+            video_timestamp INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
     return res
+
+def add_messages_to_room(roomID, timestamp, message, clientID):
+    check_query = f"SELECT COUNT(*) FROM {roomID} WHERE clientid = %s AND video_timestamp = %s AND message = %s"
+    cursor.execute(check_query, (clientID, timestamp, message))
+    result = cursor.fetchone()
+
+    if result and result[0] == 0:
+        insert_query = f"INSERT INTO {roomID} (clientid, message, video_timestamp) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (clientID, message, timestamp))    
+        connection.commit()
 
 @app.get("/api/analytics", response_class=JSONResponse)
 async def get_analytics_data():
@@ -131,11 +154,11 @@ async def get_analytics_page(request:Request):
 
 
 
-# def get_messages_for_room(room_id):
-#     query = f'SELECT * FROM {room_id}'
-#     cursor.execute(query)
-#     results = cursor.fetchall()
-#     return results
+def get_messages_from_room(room_id):
+    query = f'SELECT clientid, video_timestamp, message FROM {room_id}'
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
 
 @app.websocket("/ws/{room_id}/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str, room_id: str):
@@ -215,16 +238,30 @@ async def handle_form(video_title: str = Form(...), url: str = Form(...), profna
 @app.get("/video/{profname}/{video_title}/{new_url}/{room_id}", response_class=HTMLResponse)
 async def read_item(request: Request, profname: str, video_title: str, new_url: str, room_id: str):
     video_url = "https://www.youtube.com/embed/" + new_url + "?enablejsapi=1"
-    # messages = get_messages_for_room(room_id)
-    # print(messages)
+    stored_messages = get_messages_from_room(room_id)
+    print(stored_messages)
     messages = '';
+    def format_timestamp(timestamp):
+        hours = 0
+        minutes = 0
+        seconds = timestamp
+        while seconds >= 60:
+            seconds -= 60
+            minutes += 1
+        while minutes >= 60:
+            minutes -= 60
+            hours += 1
+        return f"{minutes:01d}:{seconds:02d}"
+    
     return templates.TemplateResponse("video.html", 
     {"request": request, 
     "video_title": video_title, 
     "profname": profname, 
     "url": video_url, 
     "messages":messages,
-    "room_id": room_id})
+    "room_id": room_id,
+    "stored_messages": stored_messages, 
+    'formatTimestamp': format_timestamp})
 
 @app.on_event("shutdown")
 async def shutdown_event():
